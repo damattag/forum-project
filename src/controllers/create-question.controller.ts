92ce1d5a9b02d1449d93b70b5f19f8f1b449a1c2
damattag <guilherme.damatta@yahoo.com.br>
2024-10-27 18:48:16 -0300

    feat: create question controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,21 +1,41 @@
-import { Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { UserPayload } from 'src/auth/jwt.strategy';
+import {
+	CreateQuestionBodySchema,
+	createQuestionBodyValidationSchema,
+} from 'src/dtos/create-question.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('/questions')
 @UseGuards(AuthGuard('jwt'))
 export class CreateQuestionController {
-	constructor(
-		private readonly jwt: JwtService,
-		private readonly prisma: PrismaService,
-	) {}
+	constructor(private readonly prisma: PrismaService) {}
 
 	@Post()
 	@HttpCode(HttpStatus.CREATED)
-	async handle(@CurrentUser() user: UserPayload) {
-		return 'ok';
+	async handle(
+		@CurrentUser() user: UserPayload,
+		@Body(createQuestionBodyValidationSchema) body: CreateQuestionBodySchema,
+	) {
+		const { title, content } = body;
+
+		await this.prisma.question.create({
+			data: {
+				title,
+				content,
+				slug: this.convertToSlug(title),
+				authorId: user.sub,
+			},
+		});
+	}
+
+	private convertToSlug(title: string) {
+		return title
+			.normalize('NFD')
+			.replace(/[\u0300-\u036f]/g, '')
+			.replace(/[^\w\s~]/g, '')
+			.replace(/\s+/g, '-');
 	}
 }
---
+++ src/dtos/create-question.dto.ts
@@ -0,0 +1,13 @@
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
+import { z } from 'zod';
+
+export const createQuestionBodySchema = z.object({
+	title: z.string(),
+	content: z.string(),
+});
+
+export type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
+
+export const createQuestionBodyValidationSchema = new ZodValidationPipe(
+	createQuestionBodySchema,
+);
