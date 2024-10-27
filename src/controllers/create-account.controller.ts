67cf537840d9cf22476105f2f7a3a24f38a58ab9
damattag <guilherme.damatta@yahoo.com.br>
2024-10-27 16:30:46 -0300

    feat: dtos

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,19 +1,8 @@
 import { ConflictException } from "@nestjs/common";
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { hash } from "bcryptjs";
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { createAccountBodyValidationSchema, CreateAccountBodySchema } from "src/dtos/create-account.dto";
 import { PrismaService } from "src/prisma/prisma.service";
-import { z } from "zod";
-
-const createAccountBodySchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
-});
-
-type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
-
-const createAccountBodyValidationSchema = new ZodValidationPipe(createAccountBodySchema);
 
 @Controller("accounts")
 export class CreateAccountController {
