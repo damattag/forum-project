0477d662f568370fde005248e1fb8d85a2fccd51
damattag <guilherme.pdamatta@gmail.com>
2024-12-27 19:06:06 -0300

    refactor: register students controller

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,45 +1,19 @@
-import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student.usecase';
 import {
 	type CreateAccountBodySchema,
 	createAccountBodyValidationSchema,
 } from '@/infra/http/dtos/create-account.dto';
-import {
-	Body,
-	ConflictException,
-	Controller,
-	HttpCode,
-	HttpStatus,
-	Post,
-} from '@nestjs/common';
-import { hash } from 'bcryptjs';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 
 @Controller('/accounts')
 export class CreateAccountController {
-	constructor(private readonly prisma: PrismaService) {}
+	constructor(private readonly useCase: RegisterStudentUseCase) {}
 
 	@Post()
 	@HttpCode(HttpStatus.CREATED)
 	async handle(@Body(createAccountBodyValidationSchema) body: CreateAccountBodySchema) {
 		const { name, email, password } = body;
 
-		const userWithSameEmail = await this.prisma.user.findUnique({
-			where: {
-				email,
-			},
-		});
-
-		if (userWithSameEmail) {
-			throw new ConflictException('User with this email already exists');
-		}
-
-		const hashedPassword = await hash(password, 8);
-
-		return this.prisma.user.create({
-			data: {
-				email,
-				name,
-				password: hashedPassword,
-			},
-		});
+		await this.useCase.execute({ name, email, password });
 	}
 }
