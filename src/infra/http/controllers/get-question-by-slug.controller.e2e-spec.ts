cc855536065a6e004d15b4fae55ef12d828fce25
damattag <guilherme.pdamatta@gmail.com>
2025-02-27 16:28:28 -0300

    feat: improve factories

--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,47 +1,47 @@
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
 import { AppModule } from '@/infra/app.module';
+import { DatabaseModule } from '@/infra/database/database.module';
 import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { INestApplication } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Test } from '@nestjs/testing';
 import request from 'supertest';
+import { QuestionFactory } from 'test/factories/make-question';
+import { StudentFactory } from 'test/factories/make-student';
 
 describe('Fetch recent questions (E2E)', () => {
 	let app: INestApplication;
 	let prisma: PrismaService;
+	let studentFactory: StudentFactory;
+	let questionFactory: QuestionFactory;
 	let jwt: JwtService;
 
 	beforeAll(async () => {
 		const moduleRef = await Test.createTestingModule({
-			imports: [AppModule],
+			imports: [AppModule, DatabaseModule],
+			providers: [StudentFactory, QuestionFactory],
 		}).compile();
 
 		app = moduleRef.createNestApplication();
 		prisma = moduleRef.get(PrismaService);
+		studentFactory = moduleRef.get(StudentFactory);
+		questionFactory = moduleRef.get(QuestionFactory);
 		jwt = moduleRef.get(JwtService);
 
 		await app.init();
 	});
 
 	test('[GET] /questions/slug/:slug', async () => {
-		const user = await prisma.user.create({
-			data: {
-				name: 'John Doe',
-				email: 'john.doe@example.com',
-				password: '123456',
-			},
-		});
+		const user = await studentFactory.makePrismaStudent();
 
 		const accessToken = jwt.sign({
-			sub: user.id,
+			sub: user.id.toString(),
 		});
 
-		await prisma.question.create({
-			data: {
-				title: 'What is the meaning of life?',
-				content: 'The meaning of life, the universe, and everything',
-				authorId: user.id,
-				slug: 'what-is-the-meaning-of-life',
-			},
+		await questionFactory.makePrismaQuestion({
+			authorId: user.id,
+			title: 'What is the meaning of life?',
+			slug: Slug.create('what-is-the-meaning-of-life'),
 		});
 
 		const response = await request(app.getHttpServer())
--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -3,7 +3,10 @@ import {
 	AnswerComment,
 	type AnswerCommentProps,
 } from '@/domain/forum/enterprise/entities/answer-comment.entity';
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/answer-comment-mapper';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { faker } from '@faker-js/faker';
+import { Injectable } from '@nestjs/common';
 
 export function makeAnswerComment(
 	override: Partial<AnswerCommentProps> = {},
@@ -20,4 +23,21 @@ export function makeAnswerComment(
 	);
 
 	return answerComment;
+}
+
+@Injectable()
+export class AnswerCommentFactory {
+	constructor(private readonly prisma: PrismaService) {}
+
+	async makePrismaAnswerComment(
+		data: Partial<AnswerCommentProps> = {},
+	): Promise<AnswerComment> {
+		const answerComment = makeAnswerComment(data);
+
+		await this.prisma.comment.create({
+			data: PrismaAnswerCommentMapper.toPersistence(answerComment),
+		});
+
+		return answerComment;
+	}
 }
